6d7020e71820607cf3801c92fb9ac895c83148fe
ggonza55 <ggonza55@uic.edu>
2022-04-19 11:16:07 -0500

    Cleaned up template code

--- src/main/java/GuiServer.java
+++ src/main/java/GuiServer.java
@@ -1,4 +1,3 @@
-
 import java.util.HashMap;
 
 import javafx.application.Application;
@@ -10,28 +9,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class GuiServer extends Application{
-
-	
-	TextField s1,s2,s3,s4, c1;
-	Button serverChoice,clientChoice,b1;
+	ListView<String> clientDialogueView, serverDialogueView, userList;
 	HashMap<String, Scene> sceneMap;
-	GridPane grid;
-	HBox buttonBox;
-	VBox clientBox;
-	Scene startScene;
-	BorderPane startPane;
 	Server serverConnection;
 	Client clientConnection;
-	
-	ListView<String> listItems, listItems2;
-	
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -41,55 +28,46 @@ public class GuiServer extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("The Networked Client/Server GUI Example");
 		
-		this.serverChoice = new Button("Server");
-		this.serverChoice.setStyle("-fx-pref-width: 300px");
-		this.serverChoice.setStyle("-fx-pref-height: 300px");
+		//Server Button
+		Button serverChoice = new Button("Server");
+		serverChoice.setStyle("-fx-pref-width: 300px");
+		serverChoice.setStyle("-fx-pref-height: 300px");
+		//Client Button
+		Button clientChoice = new Button("Client");
+		clientChoice.setStyle("-fx-pref-width: 300px");
+		clientChoice.setStyle("-fx-pref-height: 300px");
 		
-		this.serverChoice.setOnAction(e->{ primaryStage.setScene(sceneMap.get("server"));
-											primaryStage.setTitle("This is the Server");
-				serverConnection = new Server(data -> {
-					Platform.runLater(()->{
-						listItems.getItems().add(data.toString());
-					});
-
-				});
-											
-		});
+		HBox buttonBox = new HBox(400, serverChoice, clientChoice);
 		
-		
-		this.clientChoice = new Button("Client");
-		this.clientChoice.setStyle("-fx-pref-width: 300px");
-		this.clientChoice.setStyle("-fx-pref-height: 300px");
-		
-		this.clientChoice.setOnAction(e-> {primaryStage.setScene(sceneMap.get("client"));
-											primaryStage.setTitle("This is a client");
-											clientConnection = new Client(data->{
-							Platform.runLater(()->{listItems2.getItems().add(data.toString());
-											});
-							});
-							
-											clientConnection.start();
-		});
-		
-		this.buttonBox = new HBox(400, serverChoice, clientChoice);
-		startPane = new BorderPane();
-		startPane.setPadding(new Insets(70));
-		startPane.setCenter(buttonBox);
-		
-		startScene = new Scene(startPane, 800,800);
-		
-		listItems = new ListView<String>();
-		listItems2 = new ListView<String>();
-		
-		c1 = new TextField();
-		b1 = new Button("Send");
-		b1.setOnAction(e->{clientConnection.send(c1.getText()); c1.clear();});
+		clientDialogueView = new ListView<String>();
+		serverDialogueView = new ListView<String>();
 		
 		sceneMap = new HashMap<String, Scene>();
-		
 		sceneMap.put("server",  createServerGui());
 		sceneMap.put("client",  createClientGui());
-		
+
+		//When pressing the server button
+		serverChoice.setOnAction(e->{
+			primaryStage.setScene(sceneMap.get("server"));
+			primaryStage.setTitle("This is the Server");
+				serverConnection = new Server(data -> {
+					Platform.runLater(()->{
+						clientDialogueView.getItems().add(data.toString());
+					});
+				});
+		});
+		//When pressing the client button
+		clientChoice.setOnAction(e-> {
+			primaryStage.setScene(sceneMap.get("client"));
+			primaryStage.setTitle("This is a client");
+			clientConnection = new Client(data->{
+				Platform.runLater(()->{
+					serverDialogueView.getItems().add(data.toString());
+				});
+			});
+			clientConnection.start();
+		});
+		//To ensure closing the window ends the program
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent t) {
@@ -97,33 +75,36 @@ public class GuiServer extends Application{
                 System.exit(0);
             }
         });
-		
-		 
-		
+
+		//Set Start Scene and begin
+		BorderPane startPane = new BorderPane();
+		startPane.setPadding(new Insets(70));
+		startPane.setCenter(buttonBox);
+		Scene startScene = new Scene(startPane, 800,800);
 		primaryStage.setScene(startScene);
 		primaryStage.show();
-		
 	}
 	
 	public Scene createServerGui() {
-		
 		BorderPane pane = new BorderPane();
 		pane.setPadding(new Insets(70));
 		pane.setStyle("-fx-background-color: coral");
-		
-		pane.setCenter(listItems);
-	
+		pane.setCenter(clientDialogueView);
 		return new Scene(pane, 500, 400);
-		
-		
 	}
 	
 	public Scene createClientGui() {
-		
-		clientBox = new VBox(10, c1,b1,listItems2);
-		clientBox.setStyle("-fx-background-color: blue");
+		TextField c1 = new TextField();
+		Button b1 = new Button("Send");
+		b1.setOnAction(e->{
+			clientConnection.send(c1.getText());
+			c1.clear();
+		});
+		VBox chatBox = new VBox(10, c1,b1,serverDialogueView);
+		VBox usersBox = new VBox(userList);
+		chatBox.setStyle("-fx-background-color: blue");
+		HBox clientBox = new HBox(chatBox, usersBox);
 		return new Scene(clientBox, 400, 300);
-		
 	}
 
 }
